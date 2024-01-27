5d59ef00486721c0fd678fb1a41495026774a494
TunaWeiWei <somacruz23759139@gmail.com>
2024-01-28 05:58:23 +0800

    1/28
    目前功能暫時卡住，需要重新思考連結的架構及設計

--- JavaScript/service.js
+++ JavaScript/service.js
@@ -1,16 +1,26 @@
-$('#buttonPress').click(function () {
-    $.ajax({
-        url: 'https://script.google.com/macros/s/AKfycby4ewWrVVGYRGVA1Hhrnc6ewWJlP7M9kbBLpS5oS63e0UrsMdRZp-Rcz_F71j7TDs8X/exec',
-        type: 'post',
-        dataType: 'json',
-        success: function (result) {
-            alert('預約結果: ' + result.reservation_result + '/n' + result.greetWord);
 
-            window.location.replace('index.html');
 
-        },
-        error: function (xhr, status, error) {
-            console.error('Error:', status, error);
-        }
-    });
-});
+var script_url = "https://script.google.com/macros/s/AKfycby4ewWrVVGYRGVA1Hhrnc6ewWJlP7M9kbBLpS5oS63e0UrsMdRZp-Rcz_F71j7TDs8X/exec";
+$(document).ready(function ($) {
+    $("#Reservation_sheet").submit(function (event) {
+        $('#account, #userName, #customer, #date, #eatingTime').prop("disabled", false);
+        var date = $("#account").val();
+        var userName = $("#userName").val();
+        var customer = $("#customer").val();
+        var date = $("#date").val();
+        var eatingTime = $("#eatingTime").val();
+        var url = script_url + "?callback=ctrlq&account=" + account + "&userName=" + userName + "&customer=" + customer + "&date=" + date + "&eatingTime=" + eatingTime + "&action=reservation";
+        var request = $.ajax({
+            url: url,
+            type: "POST"
+        });
+        request.done(function (response, textStatus, jqXHR) {
+            window.location.reload();
+        });
+        request.always(function () {
+            $('#account, #userName, #customer, #date, #eatingTime').prop("disabled", true);
+        });
+        event.preventDefault();
+    })
+})
+
