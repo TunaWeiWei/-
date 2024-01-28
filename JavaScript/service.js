8c02ef7cb75ab83798c7f18833b06f2c124263df
TunaWeiWei <somacruz23759139@gmail.com>
2024-01-29 03:16:40 +0800

    1/29
    搞定將輸入資料上傳至google app script(GAS)寫入google sheet中,之後切換頁面至首頁並將GAS的回傳值輸入session storage內

--- JavaScript/service.js
+++ JavaScript/service.js
@@ -1,26 +1,30 @@
-
-
 var script_url = "https://script.google.com/macros/s/AKfycby4ewWrVVGYRGVA1Hhrnc6ewWJlP7M9kbBLpS5oS63e0UrsMdRZp-Rcz_F71j7TDs8X/exec";
 $(document).ready(function ($) {
     $("#Reservation_sheet").submit(function (event) {
         $('#account, #userName, #customer, #date, #eatingTime').prop("disabled", false);
-        var date = $("#account").val();
+        var account = $("#account").val();
         var userName = $("#userName").val();
         var customer = $("#customer").val();
         var date = $("#date").val();
         var eatingTime = $("#eatingTime").val();
-        var url = script_url + "?callback=ctrlq&account=" + account + "&userName=" + userName + "&customer=" + customer + "&date=" + date + "&eatingTime=" + eatingTime + "&action=reservation";
+        var url = script_url + "?account=" + account + "&userName=" + userName + "&customer=" + customer + "&date=" + date + "&eatingTime=" + eatingTime + "&action=create";
         var request = $.ajax({
             url: url,
             type: "POST"
+
         });
         request.done(function (response, textStatus, jqXHR) {
-            window.location.reload();
+            alert("完成");
+
+            //將回傳值存入session storage內讓後續頁面可以再運用
+            sessionStorage.setItem("userData", JSON.stringify(response));
+            window.location.href = "index.html";
+
         });
         request.always(function () {
             $('#account, #userName, #customer, #date, #eatingTime').prop("disabled", true);
+
         });
         event.preventDefault();
     })
 })
-
